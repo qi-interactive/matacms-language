7a0fb8aa5c36a829a0db4b27954ed6df906c0ef9
Michal Fiedorowicz <michal@qi-interactive.com>
2015-10-08 18:05:18 +0100

    Migration fixed

--- migrations/m151005_120503_add_language_mapping.php
+++ migrations/m151005_120503_add_language_mapping.php
@@ -15,11 +15,13 @@ class m151005_120503_add_language_mapping extends Migration {
 		$this->createTable('{{%matacms_language_mapping}}', [
 			'Model' => Schema::TYPE_STRING . '(64) NOT NULL',
 			'ModelId' => Schema::TYPE_INTEGER . '(11) NOT NULL',
+			'DocumentId' => Schema::TYPE_STRING . '(64) NOT NULL',
 			'Language' => Schema::TYPE_STRING . '(16) NOT NULL',
 			'Grouping' => Schema::TYPE_STRING . '(32) NOT NULL',
 			]);
 
-	    $this->createIndex('ModelModelId', '{{%matacms_language_mapping}}', ['Model', 'ModelId']);
+	    $this->addPrimaryKey('PK_Key', '{{%matacms_language_mapping}}', ['Model', 'ModelId']);
+		$this->createIndex('DocumentId', '{{%matacms_language_mapping}}', 'DocumentId');
 		$this->createIndex('Grouping', '{{%matacms_language_mapping}}', 'Grouping');
 	}
 
